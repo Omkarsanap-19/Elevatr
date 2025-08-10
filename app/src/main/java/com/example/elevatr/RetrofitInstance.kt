9550b58f2e3fcccdf7387178c496c1fd49e89102
Omkar Sanap <sanapomkar685@gmail.com>
2025-08-10 11:06:59 +0530

    auth and resume fetching

--- app/src/main/java/com/example/elevatr/RetrofitInstance.kt
+++ app/src/main/java/com/example/elevatr/RetrofitInstance.kt
@@ -4,7 +4,7 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 const val base_url="https://api.scrapingdog.com/"
-const val API_KEY ="6896ea620444ed19a669203f"
+const val API_KEY ="68982a6dbaddc11f8c993920"
 
 object RetrofitInstance {
 
